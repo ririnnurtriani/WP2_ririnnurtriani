9c5bee646e73d9950a444216a0a54c3599d32706
ririnnurtriani <ririnnurtrn@gmail.com>
2023-09-27 10:22:04 +0700

    latihan_P02web

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan<h1>";
+        echo "Nama Saya Ririn Nurtriani Saya tinggal di daerah Ahmad Yani Olahraga yang saya sukai adalah Bulutangkis";
+    }
+}
