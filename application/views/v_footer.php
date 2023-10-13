6fc13974be130a801b60ae7e47a1b0c32cd163a7
ririnnurtriani <ririnnurtrn@gmail.com>
2023-10-13 13:16:35 +0700

    WP2_Pertemuan 4 (Membuat template web sederhana)

---
+++ application/views/v_footer.php
@@ -0,0 +1,6 @@
+        <footer>
+            <a href="<?php echo base_url().'index.php/web' ?>">RentalBuku</a>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ application/views/v_news.php
@@ -0,0 +1,18 @@
+<section>
+            <h1><?php echo $judul ?></h1>
+            <center>
+                <img src="<?php echo base_url() ?>assets/img/jennie.jpg" alt="">
+            </center>
+            <p align='justify'>Jennie BLACKPINK terlihat cantik dan memikat 
+                dengan memakai outfit merah dengan latar belakang bulan purnama 
+                yang indah. Meski bukan lagu baru, antusiasme atas perilisan resmi "You & Me" 
+                sangat besar karena mereka sudah menantikan hal ini setelah sekian lama menunggu.</p>
+            <p>Lagu You & Me telah dibawakan Jennie untuk pertama kalinya pada tahun lalu saat tur konser "BORN PINK".</p>
+
+<p align='justify'>Penampilan Jennie ketika membawakan lagu tersebut di atas panggung memang sangat fenomenal.</p>
+<p align='justify'>YG menerangkan kalau "You & Me" merupakan single spesial yang segera dirilis oleh idol kelahiran tahun 1996 itu.</p>
+
+<p align='justify'>Namun, penggemar menanggapi hal berbeda dengan adanya pengumuman ini.
+    Seperti yang diketahui, Jennie menjadi salah satu member BLACKPINK yang belum menandatangani 
+    kontrak baru dengan YG Entertianment. Penggemar yang mengetahui hal ini menduga bahwa single 
+    solo Jennie BLACKPINK bakal menjadi salam perpisahan dari sang idol.</p>
